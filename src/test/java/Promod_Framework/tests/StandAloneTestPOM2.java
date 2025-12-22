9b5718fe9b256002b8cd852bae1c0ddd5c30cdcb
Promod <contactpromods@gmail.com>
2025-12-22 23:24:16 +0530

    TestNg part Data driven

--- src/test/java/Promod_Framework/tests/StandAloneTestPOM2.java
+++ src/test/java/Promod_Framework/tests/StandAloneTestPOM2.java
@@ -2,27 +2,25 @@ package Promod_Framework.tests;
 
 import Promod_Framework.TestComponents.BaseTest;
 import Promod_Framework.pageobjects.*;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
-import java.time.Duration;
 import java.util.List;
 
 public class StandAloneTestPOM2 extends BaseTest {
     String testProduct= "ADIDAS ORIGINAL";
-    @Test
-    public void submitOrderTest() throws IOException {
+    @Test(dataProvider = "getData",groups = {"Purchase"})
+    public void submitOrderTest(String email,String password, String testProduct) throws IOException {
 
 
 //        LandingPage landingPage=new LandingPage(driver);
 //        landingPage.goTo();
 //        LandingPage landingPage=launchApplication();
-        ProductCatalogue productCatalogue=landingPage.loginApplication("pramod123@gmail.com","Test@1234");
+//        ProductCatalogue productCatalogue=landingPage.loginApplication("pramod123@gmail.com","Test@1234");
+        ProductCatalogue productCatalogue=landingPage.loginApplication(email,password);
         List<WebElement>products= productCatalogue.getProductList();
         productCatalogue.addProductToCart(testProduct);
         CartPage cartPage=productCatalogue.goToCartPage();
@@ -40,7 +38,10 @@ public class StandAloneTestPOM2 extends BaseTest {
         OrderPage orderPage= productCatalogue.goToOrderPage();
 
         Assert.assertTrue(orderPage.verifyOrderDisplay(testProduct));
-
-
     }
+    @DataProvider
+    public Object[][] getData(){
+        return new Object[][]{{"pramod123@gmail.com","Test@1234","ADIDAS ORIGINAL"},{"1234pramod@gmail.com","Test@1234","ZARA COAT 3"}};
+    }
+
 }
